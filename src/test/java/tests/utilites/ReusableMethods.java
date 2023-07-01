f5f3ca45c9ede6798ef72c208abff7cd58c2e49a
RaminGuliyev1984 <ramin@guliyev.com>
2023-07-02 00:44:32 +0300

    JUnit

---
+++ src/test/java/tests/utilites/ReusableMethods.java
@@ -0,0 +1,26 @@
+package tests.utilites;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
+public class ReusableMethods {
+    public static String ikinciSayfaWHD(String ilkSayfaWindowHandleDegeri, WebDriver driver){
+
+        String ikinciSayfaWHD="";
+
+        Set<String> windowHandlesSeti  = driver.getWindowHandles();
+
+        for (String each: windowHandlesSeti
+        ) {
+
+            if (!each.equals(ilkSayfaWindowHandleDegeri)){
+                ikinciSayfaWHD = each;
+            }
+        }
+
+
+        return ikinciSayfaWHD;
+    }
+}
+
